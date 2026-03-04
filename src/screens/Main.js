83340eee040a5fb738d9d8c008c8857dc67a9848
Mansoor Ali <mansoorsahito601@gmail.com>
2026-03-04 10:33:53 +0500

    Counter App complete with useSelector

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,17 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { useSelector } from 'react-redux'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { s } from 'react-native-size-matters';
+import { clearState, decrement, increment } from '../redux/counterSlice';
 
 const Main = () => {
-    const counter = useSelector(state=>state.counter);
-    console.log(counter);
+  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch();
+  console.log(counter);
   return (
-    <View>
-      <Text>Main</Text>
+    <View style={styles.container}>
+      <Text style={{ fontSize: s(25) }}>{counter.value}</Text>
+      <Text style={styles.countertext} onPress={() => dispatch(increment())}>
+        Increment
+      </Text>
+      <Text style={styles.countertext} onPress={() => dispatch(decrement())}>
+        Decrement
+      </Text>
+      <Text style={styles.countertext} onPress={() => dispatch(clearState())}>
+        Clear
+      </Text>
     </View>
-  )
-}
+  );
+};
 
 export default Main;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+  countertext: {
+    fontSize: s(22),
+    borderWidth: 1,
+    padding: s(10),
+    marginVertical: 10,
+    marginBottom: s(10),
+  },
+});
