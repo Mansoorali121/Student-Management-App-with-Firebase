337595bfa9b238908edd156e1b5afc71c455e01d
Mansoor Ali <mansoorsahito601@gmail.com>
2026-03-04 09:51:37 +0500

    React redux master

--- App.tsx
+++ App.tsx
@@ -1,14 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { Provider } from 'react-redux';
-import Mainapp from "./src/screens/Mainapp";
-import {store} from "./src/redux/store";
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <Mainapp/>
-    </Provider>
+    <View>
+      <Text>App</Text>
+    </View>
   )
 }
 
