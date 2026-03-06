a5e1ac5b02a82c05630c442125eeb35f4288bd61
Mansoor Ali <mansoorsahito601@gmail.com>
2026-03-06 11:59:48 +0500

    Folder Structure complete

---
+++ src/components/CalcButton.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CalcButton = () => {
+  return (
+    <View>
+      <Text>CalcButton</Text>
+    </View>
+  )
+}
+
+export default CalcButton;
+
+const styles = StyleSheet.create({})
---
+++ src/redux/CalculateSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+const calcuteSlice =  createSlice({
+  name:"calculate",
+  
+  initialState:{
+    value:"",
+  },
+  reducers:{
+    addVal
+  }
+
+})
