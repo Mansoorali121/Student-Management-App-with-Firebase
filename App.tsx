51b2f135020ebe75c4abc0008238c31d9671ab85
Mansoor Ali <mansoorsahito601@gmail.com>
2026-03-06 11:51:03 +0500

    Calculator app with react redux toolkit

--- App.tsx
+++ App.tsx
@@ -1,14 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { Provider } from 'react-redux';
-import Home from "./src/screens/Home";
-import {store} from './src/redux/store';
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <Home/>
-    </Provider>
+    <View>
+      <Text>App</Text>
+    </View>
   )
 }
 
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,47 +1,14 @@
-import React, { useState } from "react";
-import { View, Text, TextInput, Button, FlatList } from "react-native";
-import { useDispatch, useSelector } from "react-redux";
-import { addTask, deleteTask, clearTasks } from "../redux/taskSlice";
-import { s } from "react-native-size-matters";
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
 const Home = () => {
-  const [text, setText] = useState("");
-  const dispatch = useDispatch();
-
-  const tasks = useSelector(state => state.tasks.tasks);
-
   return (
-    <View style={{ padding: 20 , gap:s(10)}}>
-
-      <Text>Total Tasks: {tasks.length}</Text>
-
-      <TextInput
-        placeholder="Enter Task"
-        value={text}
-        onChangeText={setText}
-        style={{ borderWidth: 1, marginVertical: 10 }}
-      />
-
-      <Button title="Add Task" onPress={() => {
-        dispatch(addTask(text));
-        setText("");
-      }} />
-
-      <FlatList
-        data={tasks}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <View style={{ flexDirection: "row", justifyContent: "space-between", marginVertical: 5 }}>
-            <Text>{item.title}</Text>
-            <Button title="Delete" onPress={() => dispatch(deleteTask(item.id))} />
-          </View>
-        )}
-      />
-
-      {/* <Button title="Clear All" onPress={() => dispatch(clearTasks())} /> */}
-
+    <View>
+      <Text>Home</Text>
     </View>
-  );
-};
+  )
+}
 
 export default Home;
+
+const styles = StyleSheet.create({})
