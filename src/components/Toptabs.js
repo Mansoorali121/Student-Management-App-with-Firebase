2d8068d8dea6169be70c39b5517763fc6e1b2dbb
Mansoor Ali <mansoorsahito601@gmail.com>
2026-03-11 12:41:16 +0500

    top tabs complete

--- src/components/Toptabs.js
+++ src/components/Toptabs.js
@@ -1,22 +1,38 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import theme from '../common/Theme';
 import { s, vs } from 'react-native-size-matters';
 
 const tabsArr = ['Subjects', 'Homework', 'Library'];
 
-const Active_Bg = 'blue';
-const Active_text = 'White';
-
 const inActive_text = 'gray';
 
 const Toptabs = () => {
+  const [activetab, setActivetab] = useState('Homework');
   return (
     <View style={styles.container}>
       {tabsArr.map(tabName => {
         return (
-          <TouchableOpacity style={styles.tabbutton}>
-            <Text style={styles.tabnametext}>{tabName}</Text>
+          <TouchableOpacity
+            onPress={() => setActivetab(tabName)}
+            style={[
+              styles.tabbutton,
+              activetab === tabName && {
+                backgroundColor: theme.colors.primary,
+                borderRadius: theme.radius.round,
+                height: vs(41),
+              },
+            ]}
+          >
+            <Text
+              style={
+                activetab === tabName
+                  ? styles.activetabtext
+                  : styles.inactivetabtext
+              }
+            >
+              {tabName}
+            </Text>
           </TouchableOpacity>
         );
       })}
@@ -30,16 +46,19 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: theme.colors.background,
     borderRadius: theme.radius.md,
-    height:vs(51),
-    flexDirection:"row",
-    alignItems:"center",
-    justifyContent:"center",
-    padding:s(4)
+    height: vs(51),
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: s(4),
   },
-  tabbutton: {  height:vs(32), flex:1,
-    borderRadius:s(8),
-    justifyContent:"center",
-    alignItems:"center"
-   },
-   tabnametext:{},
+  tabbutton: {
+    height: vs(32),
+    flex: 1,
+    borderRadius: s(8),
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  activetabtext: { color: 'white' },
+  inactivetabtext: { color: inActive_text },
 });
